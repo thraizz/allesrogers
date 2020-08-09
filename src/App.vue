a44814497a63416b05e3f2661b22e7543c767129
Aron Schüler <schueleraron@googlemail.com>
2020-08-09 16:55:53 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <landing />
+  </div>
+</template>
+
+<script>
+import Landing from './components/Landing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Landing
+  }
+}
+</script>
+
+<style>
+body {
+  background-image: url(/img/index.jpg);
+  background-size: 10%;
+}
+</style>
